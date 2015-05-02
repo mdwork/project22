a96cf3b4d4b546d57ea6e31c0114803cd90be164
Zhe1ka <zheka.dzecina@gmail.com>
2015-05-02 17:19:56 +0300

    Zhe1ka

--- js/commonJs.js
+++ js/commonJs.js
@@ -2,4 +2,21 @@ $(document).ready(function () {
     $(".btn-toggle-js").on('click', function(){
         $(this).parent().parent().find('.tbl-toggle').slideToggle();
     });
+
+    $(".btn-popup").click(function(){
+        $('#bg-popup').fadeIn(300);
+        $('#box_1').fadeIn(300);
+        $('.wrap-form-popup').fadeIn(300);
+    });
+
+    $('.close-btn-popup, .wrap-form-popup').click(function (e) {
+        e.stopPropagation();
+        $('#bg-popup').fadeOut(300);
+        $('#box_1').fadeOut(300);
+        $('.wrap-form-popup').fadeOut(300);
+    });
+
+    $('.wrap-form').click(function (e) {
+        e.stopPropagation();
+    });
 });
