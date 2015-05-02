3a00a73f1cf7fe3a3daf88457bef1b236860fc46
Zhe1ka <zheka.dzecina@gmail.com>
2015-05-02 14:57:15 +0300

    Zhe1ka

--- js/commonJs.js
+++ js/commonJs.js
@@ -1,3 +1,5 @@
-jQuery(document).ready(function ($) {
-
+$(document).ready(function () {
+    $(".btn-toggle-js").on('click', function(){
+        $(this).parent().parent().find('.tbl-toggle').slideToggle();
+    });
 });
