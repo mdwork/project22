76267c2992272443056511cfc0ee586ad1b44169
Zhe1ka <zheka.dzecina@gmail.com>
2015-05-03 12:41:59 +0300

    Zhe1ka

--- js/commonJs.js
+++ js/commonJs.js
@@ -3,31 +3,26 @@ $(document).ready(function () {
         $(this).parent().parent().find('.tbl-toggle').slideToggle();
     });
 
-    $("body").append('<span class="wrap-form-ajax"></span>');
-    $('.wrap-form-ajax').load("form.html #form-ajax", function(){
-        $(".btn-popup").click(function(){
-            $('body').css('overflow','hidden');
+    $(".btn-popup").click(function(){
+        $('body').css('overflow','hidden');
 
-            $('#bg-popup, .wrap-form-popup').css('top', $(window).scrollTop())
+        $('#bg-popup, .wrap-form-popup').css('top', $(window).scrollTop());
 
-            $('#bg-popup').fadeIn(300);
-            $('#box_1').fadeIn(300);
-            $('.wrap-form-popup').fadeIn(300);
+        $('#bg-popup').fadeIn(300);
+        $('#box_1').fadeIn(300);
+        $('.wrap-form-popup').fadeIn(300);
+    });
 
+    $('.close-btn-popup, .wrap-form-popup').click(function (e) {
+        $('body').css('overflow','visible');
 
-        });
+        e.stopPropagation();
+        $('#bg-popup').fadeOut(300);
+        $('#box_1').fadeOut(300);
+        $('.wrap-form-popup').fadeOut(300);
+    });
 
-        $('.close-btn-popup, .wrap-form-popup').click(function (e) {
-            $('body').css('overflow','visible');
-
-            e.stopPropagation();
-            $('#bg-popup').fadeOut(300);
-            $('#box_1').fadeOut(300);
-            $('.wrap-form-popup').fadeOut(300);
-        });
-
-        $('.wrap-form').click(function (e) {
-            e.stopPropagation();
-        });
+    $('.wrap-form').click(function (e) {
+        e.stopPropagation();
     });
 });
